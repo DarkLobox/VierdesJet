91b787b97996485803bb4b2fb6176642d9bcff55
DarkLobox <lgomezp@unsa.edu.pe>
2022-07-02 00:51:28 -0500

    Dependencias y prueba de firebase

--- app/src/main/java/com/danp/vierdesjet/MainActivity.kt
+++ app/src/main/java/com/danp/vierdesjet/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.danp.vierdesjet.screens.Login
 import com.danp.vierdesjet.ui.theme.VierdesJetTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,26 +19,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             VierdesJetTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Login()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    VierdesJetTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/danp/vierdesjet/screens/Login.kt
@@ -0,0 +1,31 @@
+package com.danp.vierdesjet.screens
+
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
+
+@Composable
+fun Login(
+
+) {
+    val db = Firebase.firestore
+    val coroutineScope = rememberCoroutineScope()
+
+    //Prueba de firestore
+    db.collection("prueba").get().addOnSuccessListener {
+            documents -> for (document in documents){
+                coroutineScope.launch {
+                    Log.d("Prueba", document.getString("nombre").toString());
+                }
+            }
+    }
+
+    Column() {
+        Text(text = "Login")
+    }
+}
