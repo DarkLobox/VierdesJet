1be3adf728e4f3b1e852a357ae27f069a520397c
DarkLobox <lgomezp@unsa.edu.pe>
2022-07-07 08:41:42 -0500

    Carga de datos por medio de excel

--- app/src/main/java/com/danp/vierdesjet/screens/PlantsActions.kt
+++ app/src/main/java/com/danp/vierdesjet/screens/PlantsActions.kt
@@ -2,31 +2,27 @@ package com.danp.vierdesjet.screens
 
 import android.util.Log
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.toSize
 import com.danp.vierdesjet.DataStoreManager
 import com.danp.vierdesjet.R
 import com.danp.vierdesjet.room.plant.PlantApp
 import com.danp.vierdesjet.room.plant.PlantEntity
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.launch
+import org.apache.poi.ss.usermodel.Cell
+import org.apache.poi.ss.usermodel.Row
+import org.apache.poi.xssf.usermodel.XSSFRow
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import java.io.InputStream
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -156,6 +152,69 @@ fun PlantsActions(
                 }
             },modifier = Modifier.width(200.dp)) {
                 Text("RESET")
+            }
+
+            Button(onClick = {
+                val myInput: InputStream
+
+                myInput = context.assets.open("plantsdata.xlsx")
+
+                val myWorkBook = XSSFWorkbook(myInput)
+
+                val mySheet = myWorkBook.getSheetAt(0)
+                val rowIter: Iterator<Row> = mySheet.rowIterator()
+                var rowno = 0
+
+                while (rowIter.hasNext()) {
+                    Log.e("TAG", " row no $rowno")
+                    val myRow = rowIter.next() as XSSFRow
+                    if (rowno != -1) {
+                        val cellIter: Iterator<Cell> = myRow.cellIterator()
+                        var colno = 0
+                        var textType = ""
+                        var textDescription = ""
+                        var textPrice = ""
+                        var textPassword = ""
+                        while (cellIter.hasNext()) {
+                            val myCell = cellIter.next()
+                            if (colno == 0) {
+                                textType = myCell.toString()
+                            } else if (colno == 1) {
+                                textDescription = myCell.toString()
+                            } else if (colno == 2) {
+                                textPrice = myCell.toString()
+                            }
+
+                            colno++
+                            //Log.e("TAG", " Index :" + myCell.columnIndex + " -- " + myCell.toString())
+                        }
+                        val dateTemp = SimpleDateFormat("dd/M/yyyy").format(Date())
+                        val plant = PlantEntity(
+                            textType,
+                            textPrice,
+                            textDescription,
+                            false,
+                            false,
+                            false,
+                            userCode,
+                            dateTemp,
+                            true
+                        )
+                        coroutineScope.launch {
+                            try {
+                                plantApp.room.plantDao().insert(plant)
+                            } catch (e: Exception) {
+                                Log.d("Prueba", "Campos invalidos en INSERT")
+                            }
+                            Log.d("Prueba",
+                                 plant.type + "," + plant.price + "," + plant.description + "," + plant.code)
+                        }
+
+                    }
+                    rowno++
+                }
+            },modifier = Modifier.width(200.dp)) {
+                Text("LOAD DATA")
             }
         }
     }
